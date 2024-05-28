b5fa07d2a15b8b348d302b31ce7b45ef57facf71
valed-dm <dmvaled@gmail.com>
2024-05-29 01:31:30 +0300

    class 'Worker' now process data in batches:
    - collects AppsInstalled objects in batches.
    - processes each batch using the process_batch method, which uses insert_appsinstalled to insert data into memcached using set_multi.

--- utils/worker.py
+++ utils/worker.py
@@ -1,12 +1,14 @@
 import logging
 import threading
 from queue import Queue
-from typing import Dict
+from typing import Dict, List
 
 import memcache
 from tqdm import tqdm
 
-from apps.appsinstalled import parse_appsinstalled, insert_appsinstalled
+from apps.appsinstalled import AppsInstalled
+from apps.appsinstalled import insert_appsinstalled
+from apps.appsinstalled import parse_appsinstalled
 
 
 class Worker(threading.Thread):
@@ -18,7 +20,8 @@ class Worker(threading.Thread):
             results_lock: threading.Lock,
             results: Dict[str, int],
             pbar: tqdm,
-            memc_clients: Dict[str, memcache.Client]
+            memc_clients: Dict[str, memcache.Client],
+            batch_size: int = 1000
     ) -> None:
         threading.Thread.__init__(self)
         self.queue = queue
@@ -28,11 +31,15 @@ class Worker(threading.Thread):
         self.results = results
         self.pbar = pbar
         self.memc_clients = memc_clients
+        self.batch_size = batch_size
 
     def run(self) -> None:
+        batch = []
         while True:
             line = self.queue.get()
             if line is None:
+                if batch:
+                    self.process_batch(batch)
                 break
             appsinstalled = parse_appsinstalled(line)
             if not appsinstalled:
@@ -41,20 +48,26 @@ class Worker(threading.Thread):
                 self.queue.task_done()
                 self.pbar.update(1)
                 continue
-            memc_addr = self.device_memc.get(appsinstalled.dev_type)
-            if not memc_addr:
-                logging.error("Unknown device type: %s" % appsinstalled.dev_type)
-                with self.results_lock:
-                    self.results['errors'] += 1
-                self.queue.task_done()
-                self.pbar.update(1)
-                continue
-            memc_client = self.memc_clients[memc_addr]
-            if insert_appsinstalled(memc_client, appsinstalled, self.dry_run):
-                with self.results_lock:
-                    self.results['processed'] += 1
-            else:
-                with self.results_lock:
-                    self.results['errors'] += 1
+            batch.append(appsinstalled)
+            if len(batch) >= self.batch_size:
+                self.process_batch(batch)
+                batch = []
             self.queue.task_done()
             self.pbar.update(1)
+        if batch:
+            self.process_batch(batch)
+
+    def process_batch(self, batch: List[AppsInstalled]) -> None:
+        memc_addr = self.device_memc.get(batch[0].dev_type)
+        if not memc_addr:
+            logging.error("Unknown device type: %s" % batch[0].dev_type)
+            with self.results_lock:
+                self.results['errors'] += len(batch)
+            return
+        memc_client = self.memc_clients[memc_addr]
+        if insert_appsinstalled(memc_client, batch, self.dry_run):
+            with self.results_lock:
+                self.results['processed'] += len(batch)
+        else:
+            with self.results_lock:
+                self.results['errors'] += len(batch)
